4c1a616a3ea7f1ab93fdc802ffa3124fcad3f36d
Neliuswandia <nelius.wandia@student.moringaschool.com>
2025-04-05 11:37:28 -0700

    checks if password has a number

--- scripts.js
+++ scripts.js
@@ -1,4 +1,38 @@
-const form = document.getElementsByTagName("form");
+// const form = document.querySelector("form");
+// const password = document.getElementById("password");
+// const passwordError = document.getElementById("passwordError");
+
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     const passwordValue = password.value;
+//     let hasUpperCase = false;
+//     let hasNumber = false;
+
+//     for (let i = 0; i < passwordValue.length; i++) {
+//         if (passwordValue[i] >= 'A' && passwordValue[i] <= 'Z') {
+//           hasUpperCase = true;
+//           break;
+//         }
+//       }
+
+//     for (let i = 0; i < passwordValue.length; i++) {
+//         if (passwordValue[i] >= '0' && passwordValue[i] <= '9') {
+//           hasNumber = true;
+//           break;
+//         }
+//       }
+//     if (hasUpperCase && hasNumber) {
+//       passwordError.textContent = "Password is valid!";
+//       passwordError.style.color = "green";
+//     }
+//     else {
+//       passwordError.textContent = "Password must contain at least one uppercase letter and one number.";
+//       passwordError.style.color = "red";
+//     }
+// })
+
+const form = document.querySelector("form");
 const password = document.getElementById("password");
 const passwordError = document.getElementById("passwordError");
 
@@ -6,21 +40,14 @@ form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     const passwordValue = password.value;
-    let hasUppercase = false;
-    let hasNumber = false;
+    let hasUpperCase = /[A-Z]/.test(passwordValue);
+    let hasNumber = /[0-9]/.test(passwordValue);
 
-    for (let i = 0; i < password.length; i++) {
-        if (password[i] >= 'A' && password[i] <= 'Z') {
-          hasUpperCase = true;
-          break;
-        }
-      }
-      
-    for (let i = 0; i < password.length; i++) {
-        if (password[i] >= 'A' && password[i] <= 'Z') {
-          hasUpperCase = true;
-          break;
-        }
-      }
-
-})
+    if (hasUpperCase && hasNumber) {
+        passwordError.textContent = "Password is valid!";
+        passwordError.style.color = "green";
+    } else {
+        passwordError.textContent = "Password must contain at least one uppercase letter and one number.";
+        passwordError.style.color = "red";
+    }
+});
